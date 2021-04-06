00f4162e65869364f9230edcefe8e831d6d6f973
DOUSLIMI Yasser <ignema5@hotmail.com>
2021-04-06 19:17:16 +0100

    Added basic navigation menu with fragment integration

--- app/src/main/java/com/android/leafter/MainActivity.java
+++ app/src/main/java/com/android/leafter/MainActivity.java
@@ -15,16 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+//        bnv.getMenu().getItem(2).setEnabled(false);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.miHome, R.id.miMusic, R.id.miCatalogue)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
+
 
 }
