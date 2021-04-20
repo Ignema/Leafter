bccdce0bdd0133eebeec7f5ec5150f9ae6f17478
Amine CHICHI <aminechichi99@gmail.com>
2021-04-20 02:34:59 +0000

    Working on the media player layout

---
+++ app/src/main/java/com/android/leafter/MusicPlayer_Activity.java
@@ -0,0 +1,14 @@
+package com.android.leafter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPlayer_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player_);
+    }
+}
