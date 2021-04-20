91cb6b337c2129d2ae3e31a82aa7941eb3857394
Amine CHICHI <aminechichi99@gmail.com>
2021-04-20 17:43:05 +0000

    minor modifications play pause icons

--- app/src/main/java/com/android/leafter/MusicPlayer_Activity.java
+++ app/src/main/java/com/android/leafter/MusicPlayer_Activity.java
@@ -2,13 +2,52 @@ package com.android.leafter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MusicPlayer_Activity extends AppCompatActivity {
+public class MusicPlayer_Activity extends AppCompatActivity implements View.OnClickListener {
+
+//    Views delcaration
+
+    TextView tvTime, tvDuration;
+    Button btnPlay;
+    SeekBar seekBarTime, seekBarVolume;
+
+    MediaPlayer musicPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player_);
+
+        tvTime = findViewById(R.id.tvTime);
+        tvDuration = findViewById(R.id.tvDuration);
+        btnPlay = findViewById(R.id.btnPlay);
+        seekBarTime = findViewById(R.id.seekBar_time);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+
+        musicPlayer = MediaPlayer.create(this, R.raw.mami_sayada);
+        musicPlayer.setLooping(true);
+        musicPlayer.seekTo(0);
+        musicPlayer.setVolume(0.5f, 0.5f);
+
+        btnPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnPlay){
+            if(musicPlayer.isPlaying()){
+                musicPlayer.pause();
+                btnPlay.setBackgroundResource(R.drawable.ic_play);
+            }else{
+                musicPlayer.start();
+                btnPlay.setBackgroundResource(R.drawable.ic_pause);
+            }
+        }
     }
 }
