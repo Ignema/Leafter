8026f967424e3a9be9459d712b88d1b27d71e91b
Amine CHICHI <aminechichi99@gmail.com>
2021-04-21 01:52:27 +0000

    dynamically change the volume seekbar and music control seekbar

--- app/src/main/java/com/android/leafter/MusicFragment.java
+++ app/src/main/java/com/android/leafter/MusicFragment.java
@@ -1,5 +1,6 @@
 package com.android.leafter;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MusicFragment extends Fragment {
 
-
+    Button btnMusic;
     public MusicFragment() {
         // Required empty public constructor
     }
@@ -21,6 +24,22 @@ public class MusicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false);
+        View musicFragmentView = inflater.inflate(R.layout.fragment_music, container, false);
+
+        return musicFragmentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        btnMusic = (Button) getActivity().findViewById(R.id.btnMusic);
+        btnMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MusicPlayer_Activity.class);
+                startActivity(intent);
+//                Toast.makeText(getContext(), "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/android/leafter/MusicPlayer_Activity.java
+++ app/src/main/java/com/android/leafter/MusicPlayer_Activity.java
@@ -2,11 +2,16 @@ package com.android.leafter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MusicPlayer_Activity extends AppCompatActivity implements View.OnClickListener {
@@ -18,6 +23,7 @@ public class MusicPlayer_Activity extends AppCompatActivity implements View.OnCl
     SeekBar seekBarTime, seekBarVolume;
 
     MediaPlayer musicPlayer;
+    AudioManager audiomanager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +42,115 @@ public class MusicPlayer_Activity extends AppCompatActivity implements View.OnCl
         musicPlayer.setVolume(0.5f, 0.5f);
 
         btnPlay.setOnClickListener(this);
+        initVolumeControls();
+        initMusicControls();
+    }
+
+    private void initMusicControls() {
+        seekBarTime.setMax(musicPlayer.getDuration());
+        tvDuration.setText(millisecondsToString(musicPlayer.getDuration()));
+        seekBarTime.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean isFromUser) {
+                if(isFromUser){
+                    musicPlayer.seekTo(progress);
+                    seekBarTime.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (musicPlayer != null){
+                    if(musicPlayer.isPlaying()){
+                        try {
+                            final double current = musicPlayer.getCurrentPosition();
+                            String elapsedTime = millisecondsToString((int) current);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTime.setText(elapsedTime);
+                                    seekBarTime.setProgress((int) current);
+                                }
+                            });
+                            Thread.sleep(1000);
+
+                        }catch (InterruptedException e){
+
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void initVolumeControls() {
+        audiomanager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        seekBarVolume.setMax(audiomanager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBarVolume.setProgress(audiomanager.getStreamVolume(AudioManager.STREAM_MUSIC));
+        seekBarVolume.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean isFronUser) {
+                audiomanager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+    private String millisecondsToString(int duration) {
+        String elapsedTime = "";
+        int minutes = duration / 1000 / 60;
+        int seconds = duration / 1000 % 60;
+        elapsedTime += minutes + ":";
+        if (seconds < 10) elapsedTime += "0";
+        elapsedTime += seconds;
+        return elapsedTime;
+    }
+
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            int index = seekBarVolume.getProgress();
+            seekBarVolume.setProgress(index + 1);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            int index = seekBarVolume.getProgress();
+            seekBarVolume.setProgress(index - 1);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.btnPlay){
-            if(musicPlayer.isPlaying()){
+        if (view.getId() == R.id.btnPlay) {
+            if (musicPlayer.isPlaying()) {
                 musicPlayer.pause();
                 btnPlay.setBackgroundResource(R.drawable.ic_play);
-            }else{
+            } else {
                 musicPlayer.start();
                 btnPlay.setBackgroundResource(R.drawable.ic_pause);
             }
